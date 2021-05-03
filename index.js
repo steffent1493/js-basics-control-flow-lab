ba0eb722bc685a341432bbf6c1e162fcdb631162
steffent1493 <steffent1493@gmail.com>
2021-05-03 12:55:00 -0400

    mission accomplished

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+if  (someNumber <= 400)
+  return "This one is on me!"
+else if (someNumber <= 2499)
+  return (`I will gladly take your thirty bucks.`)
+else if (someNumber >= 2500)
+  return (`No can do.`)
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  // Write your code here
+return ternaryCheckCity = city === "NYC" ? `Ok, sounds good.` : `No go.`
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case `generous` :
+    return `Thank you so much.`;
+    break;
+
+    case `not as generous` :
+      return `Thank you.`
+      break;
+
+    default :
+    return `Bye.`
+    break;
+
+  }
 }
